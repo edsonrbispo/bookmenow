38f1fa1cddf041902805e4fb275c154d3cda65ce
Edson Rodrigues <edsonrb.2008@gmail.com>
2023-11-10 21:59:32 -0300

    Personalização responsiva

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const botaoMenu = document.getElementById('botao-menu-mobile');
+const menu = document.getElementById('menu');
+
+botaoMenu.addEventListener('click', function () {
+    menu.classList.toggle('menu-ativo');
+});
